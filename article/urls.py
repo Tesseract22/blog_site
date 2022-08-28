c1b504ce8a45411d691304df53d23828c95cb583
Tesseract22 <caleb2222@qq.com>
2022-08-28 21:28:32 +0000

    article details in progress

--- article/urls.py
+++ article/urls.py
@@ -2,9 +2,9 @@ app_name = 'article'
 
 from django.conf.urls import url
 from . import views
-
+from django.urls import path
 
 urlpatterns = [
-    url('', views.view_articles)
-
+    url('view/', views.main),
+    path('view-article/<int:id>/', views.view_article, name="view-article"),
 ]
--- article/views.py
+++ article/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
 
+import article
+
 
 
 # Create your views here.
 from .models import Article
 
 
-def article_main(request):
-    return render(request, "article_main.html")
 
-def view_articles(request):
+def main(request):
     articles = Article.objects.all()
 
     ctx = {}
     ctx["articles"] = articles
 
     return render(request, 'article/article_main.html', ctx)
+
+
+def view_article(request, id):
+    article = Article.objects.get(id=id)
+    ctx = {}
+    ctx['article'] = article
+
+    return render(request, 'article/article_detail.html', ctx)
