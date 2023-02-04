fcbcc85095ca1a3aff9c62195e2314b4352dc465
Tesseract22 <caleb2222@qq.com>
2023-02-04 17:08:51 +0000

    chat

--- blog_site/chat.py
+++ blog_site/chat.py
@@ -2,26 +2,42 @@ import openai
 import json
 from django.http import HttpResponse
 from django.shortcuts import render
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 
 
 def Send(request):
+    q = request.POST["q"]
+    print("query: %s"%q)
+    q1 = "Q: %s专业可以有哪些职业发展方向? A:"%q
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt="Q: 土木工程 A:",
+        prompt=q1,
         temperature=0,
-        max_tokens=100,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        
+    )
+    # r = json.loads(response)
+    a1 = response['choices'][0]["text"]
+    q2 = q1 + a1 + "Q: 需要学习哪些知识来胜任这些职位? A:"
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=q2,
+        temperature=0,
+        max_tokens=1000,
         top_p=1,
         frequency_penalty=0.0,
         presence_penalty=0.0,
         stop=["\n"]
     )
-    # r = json.loads(response)
-    init = response['choices'][0]["text"]
-    print(init)
-
-    # return HttpResponse(json.dumps({"init":init}), content_type="application/json")
-Send(5)
+    a2 = response['choices'][0]["text"]
+    print(q1)
+    print(q2)
+    # print(a1, a2)
+    return HttpResponse(json.dumps({"a1":a1, "a2":a2}), content_type="application/json")
+# Send(5)
 
