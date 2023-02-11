a1a55beff5cff3df54f395197c3efa29ae4264ee
Tesseract22 <caleb2222@qq.com>
2023-02-11 17:20:12 +0000

    get rid of unsed import

--- blog_site/chat.py
+++ blog_site/chat.py
@@ -1,30 +1,20 @@
-import openai
-import json
+
 from django.http import StreamingHttpResponse 
 from django.shortcuts import render
-import re
 import os
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
-
-
 import requests
-
 import json
 import pprint
-# pip install sseclient-py
 import sseclient    
-import logging
 import sys
-# openai.api_key = settings.API_KEY
+
 @csrf_exempt
 def Send(request):
-    # print(request.body.decode('utf-8'), file=sys.stderr, flush=True)
-    print(request)
     q = request.GET["q"]
-    # print(request.GET, q, file=sys.stderr, flush=True)
     return StreamingHttpResponse(streaming_content=performRequestWithStreaming(q), content_type='text/event-stream')
-# Send(5)
+
 
 def testIter():
     for i in range(1000):
@@ -48,8 +38,10 @@ def performRequestWithStreaming(q):
     for event in client.events():
         if event.data != '[DONE]':
             txt = json.loads(event.data)['choices'][0]['text']
-            print(txt)
+            print(txt, file=sys.stderr, flush=True)
             yield f"data: {txt}\n\n"
+        else:
+            yield f"data: {settings.SSE_STOP_SIGNAL}\n\n"
 
 if __name__ == '__main__':
     for i in performRequestWithStreaming("Can you explain in datail the sino-japanese war"):
